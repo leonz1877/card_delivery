878e2f36015973b86f99ead1cf878c4ec1c18d02
Leonid Zhdanov <zhdanov.leon@gmail.com>
2022-08-13 18:39:27 +0400

    create test

---
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -0,0 +1,61 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class CardDeliveryTest {
+    /*public LocalDate meetDate() {
+        LocalDate data = LocalDate.parse(LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+        return data;
+    };
+
+    String date = String.valueOf(meetDate());*/
+
+    public String today (int day) {
+        if (day < 3) {
+            day = 3;
+        }
+        return LocalDate.now().plusDays(day).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    String meetDate = today(7);
+
+    @Test
+    void cardDeliveryTest() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browser = "firefox";
+
+        open("http://localhost:9999");
+        $("[data-test-id = city] input").setValue("Ижевск");
+        $("[data-test-id = date] input").doubleClick();
+        $("[data-test-id = date] input").sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id = date] input").setValue((meetDate));
+        $("[data-test-id = name] input").setValue("Уткин Василий");
+        $("[data-test-id = phone] input").setValue("+79398887766");
+        $("[data-test-id = agreement]").click();
+        $$(By.className("button__content")).first().click();
+        $("[data-test-id = notification]").shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $x(".//div[@class='notification__title']"). shouldBe(text("Успешно!"));
+        $x(".//div[@class='notification__content']").shouldBe(text("Встреча успешно забронирована на " + meetDate));
+
+
+
+
+
+
+
+
+
+
+    }
+}
